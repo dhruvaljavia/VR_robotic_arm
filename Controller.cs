21e60f58f25c4be622e607ceebb82cf8ca063377
Dhruval javia <46935683+dhruvaljavia@users.noreply.github.com>
2019-09-07 12:31:00 +0530

    Add files via upload

---
+++ Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float x,y,z;
+    private float Th, Ph, R;
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = 0.0001f;
+        y = -1f;
+        z = 0.0001f;
+        Th = - Mathf.Rad2Deg * (Mathf.Atan(y / x));
+        Ph = Mathf.Rad2Deg * (Mathf.Atan(x / (z * Mathf.Cos(Mathf.Atan(y / x)))));
+        R = z / Mathf.Cos((Mathf.Atan(x / (z * Mathf.Cos(Mathf.Atan(y / x))))));
+        Debug.Log(Th);
+        Debug.Log(Ph);
+        Debug.Log(R);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ changeLength.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeLength: MonoBehaviour
+{
+    
+    private GameObject Arm2;
+    HingeJoint hinge;
+    JointMotor motor;
+    public float a3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hinge = GetComponent<HingeJoint>();
+        motor = hinge.motor;
+        motor.force = 10;
+        motor.targetVelocity = -30;
+        motor.freeSpin = false;
+        hinge.useMotor = true;
+        Arm2 = GameObject.Find("Arm2");
+        a3 = 90;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Arm2.transform.localEulerAngles.x >= (a3-1.5) && Arm2.transform.localEulerAngles.x <= a3)
+        {
+            hinge.useMotor = false;
+            
+            
+        }
+        if (Arm2.transform.localEulerAngles.x > a3)
+        {
+            hinge.useMotor = true;
+            motor.targetVelocity = 30;
+        }
+        if (Arm2.transform.localEulerAngles.x < (a3-1.5))
+        {
+            hinge.useMotor = true;
+            motor.targetVelocity = -30;
+        }
+        //Debug.Log(Arm2.transform.localEulerAngles.x);
+    }
+}
