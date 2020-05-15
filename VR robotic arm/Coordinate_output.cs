89e34d4a33b57c576144dbf17bd2522daf95bdd0
Dhruval javia <46935683+dhruvaljavia@users.noreply.github.com>
2020-05-15 23:09:23 +0530

    Add files via upload

---
+++ VR robotic arm/Coordinate_output.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class Coordinate_output : MonoBehaviour
+{
+	private GameObject armRotar2;
+	public Vector3 pos;
+	SerialPort sp = new SerialPort("COM7", 9600);
+    // Start is called before the first frame update
+    void Start()
+    {
+        armRotar2 = GameObject.Find("armRotar2");
+        sp.Open();
+        sp.ReadTimeout = 200;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = armRotar2.transform.position;
+        float a = ((float)(pos.x * 1000.0) - (float)(126.0))/((float)(2371.0))* ((float)(26.7));
+        float b = (float)(pos.y * 100.0)/((float)(276.0))*((float)(26.7)) ;
+        float c = ((float)(pos.z * 1000.0) - (float)(431.0))/((float)(2371.0)) * ((float)(26.7));
+        string x = (a).ToString();
+        string y = (b).ToString();
+        string z = (c).ToString();
+        //string y = pos.y.ToString();
+        //string z = pos.z.ToString();
+        //string p = x + " " + y + " " + z;
+        sp.WriteLine(x);
+        sp.WriteLine(y);
+        sp.WriteLine(z);
+        Debug.Log(y);
+    }
+}
---
+++ VR robotic arm/rotArm2.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotArm2 : MonoBehaviour
+{
+    private GameObject Arm2;
+    HingeJoint hinge;
+    JointMotor motor;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hinge = GetComponent<HingeJoint>();
+        rb = GetComponent<Rigidbody>();
+        motor = hinge.motor;
+        motor.force = 10;
+        motor.targetVelocity = 10;
+        motor.freeSpin = false;
+        hinge.useMotor = false;
+        Arm2 = GameObject.Find("Arm2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.constraints = RigidbodyConstraints.None;
+            hinge.useMotor = true;
+            motor.force = 1000;
+            motor.targetVelocity = -50;
+            hinge.motor = motor;
+
+
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.constraints = RigidbodyConstraints.None;
+            hinge.useMotor = true;
+            motor.force = 1000;
+            motor.targetVelocity = 50;
+            hinge.motor = motor;
+
+        }
+        else
+        {
+            hinge.useMotor = false;
+            rb.constraints = RigidbodyConstraints.FreezeRotationX;
+        }
+    }
+}
